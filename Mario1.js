f7f574e0b89dc39f33c756c0d57ebf801d18ae47
OmiCremer <87448345+OmiCremer@users.noreply.github.com>
2021-07-16 18:20:12 -0300

    Update Mario1.js
    incorporo para que tenga fecha

--- Mario1.js
+++ Mario1.js
@@ -4,3 +4,13 @@ function suma(a,b){
 }
 let sumaDeDosNumeros = suma(50,75);
 console.log(sumaDeDosNumeros);
+
+let fechaActual = new Date ();
+
+let anio = fechaActual.getFullYear();
+let mes = fechaActual.getMonth();
+let dia = fechaActual.getDate();
+
+let meses = ["Enero", "Febrero","Marzo","Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log ("Hoy es el " + dia + " de " + meses[mes] + " de " + anio );
